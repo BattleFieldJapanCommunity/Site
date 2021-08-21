7da4a86d952d3988c90c341b49322d18dc72d744
NaruseNia <67743327+NaruseNia@users.noreply.github.com>
2021-08-21 20:31:20 +0900

    Gulpに合わせたファイル構造の変更と細かな修正 (#6)
    * Gulpに合わせたファイル構造の変更と細かな修正
    
    * EOF修正
    
    * watchTask修正

--- gulpfile.js
+++ gulpfile.js
@@ -11,61 +11,61 @@ const browsersync = require("browser-sync");
 
 //scssからcssに変換
 const scssTask = () => {
-  return src("./docs/scss/**/*.scss")
+  return src("./src/scss/**/*.scss")
     .pipe(sass())
     .pipe(postcss([cssnano()]))
-    .pipe(changed("./public/css"))
-    .pipe(dest("./public/css"));
+    .pipe(changed("./docs/css"))
+    .pipe(dest("./docs/css"));
 };
 
 //JSファイルの最適化
 const jsTask = () => {
-  return src("./docs/**/*.js")
+  return src("./src/js/**/*.js")
     .pipe(terser())
-    .pipe(changed("./public/js"))
-    .pipe(dest("./public/js"));
+    .pipe(changed("./docs/js"))
+    .pipe(dest("./docs/js"));
 };
 
 //htmlをpublicに書き出し
 const htmlTask = () => {
-  return src("./docs/**/*.html")
-    .pipe(changed("./public"))
-    .pipe(dest("./public"));
+  return src("./src/**/*.html")
+    .pipe(changed("./docs"))
+    .pipe(dest("./docs"));
 };
 
 //画像の圧縮
 //gulp-imagemin ^8.0.0だと動きません。
 const imageTask = () => {
-  return src("./docs/img/**")
-    .pipe(changed("./public/img"))
+  return src("./src/img/**")
+    .pipe(changed("./docs/img"))
     .pipe(
       imagemin([
-        imagemin.svgo(),
         pngquant({
           quality: [0.6, 0.7],
           speed: 1,
         }),
         mozjpeg({ quality: 65 }),
-        imagemin.optipng,
-        imagemin.gifsicle({ optimizationLevel: 3 }),
+        imagemin.svgo(),
+        imagemin.optipng(),
+        imagemin.gifsicle(),
       ])
     )
-    .pipe(dest("./public/img"));
+    .pipe(dest("./docs/img"));
 };
 
 //監視
 const watchTask = () => {
-  watch("./docs/**/*.html", series(htmlTask, browsersyncReload));
-  watch("./docs/img/**", series(imageTask, browsersyncReload));
-  watch("./docs/scss/**/*.scss", series(scssTask, browsersyncReload));
-  watch("./docs/js/**/*.js", series(jsTask, browsersyncReload));
+  watch("./src/**/*.html", series(htmlTask, browsersyncReload));
+  watch("./src/img/**", series(imageTask, browsersyncReload));
+  watch("./src/scss/**/*.scss", series(scssTask, browsersyncReload));
+  watch("./src/js/**/*.js", series(jsTask, browsersyncReload));
 };
 
 //browsersyncの開始
 const browsersyncServe = (cb) => {
   browsersync.init({
     server: {
-      baseDir: "./public",
+      baseDir: "./docs",
     },
   });
   cb();
@@ -77,13 +77,6 @@ const browsersyncReload = (cb) => {
   cb();
 };
 
-exports.run = series(
-  htmlTask,
-  imageTask,
-  scssTask,
-  jsTask,
-  browsersyncServe,
-  watchTask
-);
+exports.run = series(htmlTask, imageTask, scssTask, jsTask, browsersyncServe, watchTask);
 
 exports.build = series(htmlTask, imageTask, scssTask, jsTask);
