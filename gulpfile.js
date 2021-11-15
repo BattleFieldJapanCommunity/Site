0a1b8360b040793eedefc630f5c3c150fc544b76
Naruse Nia <notshinsin0000@gmail.com>
2021-11-15 21:42:45 +0900

    ディレクトリ構造の単純化

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ const browsersync = require("browser-sync");
 
 //scssからcssに変換
 const scssTask = () => {
-  return src("./src/scss/**/*.scss")
+  return src("./docs/scss/**/*.scss")
     .pipe(sass())
     .pipe(postcss([cssnano()]))
     .pipe(changed("./docs/css"))
@@ -20,39 +20,16 @@ const scssTask = () => {
 
 //JSファイルの最適化
 const jsTask = () => {
-  return src("./src/js/**/*.js")
+  return src("./docs/js/**/*.js")
     .pipe(terser())
-    .pipe(changed("./docs/js"))
-    .pipe(dest("./docs/js"));
-};
-
-//htmlをpublicに書き出し
-const htmlTask = () => {
-  return src("./src/**/*.html")
-    .pipe(changed("./docs"))
-    .pipe(dest("./docs"));
-};
-
-//画像の圧縮
-//gulp-imagemin ^8.0.0だと動きません。
-const imageTask = () => {
-  return src("./src/img/**")
-    .pipe(changed("./docs/img"))
-    .pipe(
-      imagemin([
-        mozjpeg({ quality: 65 }),
-        imagemin.svgo(),
-      ])
-    )
-    .pipe(dest("./docs/img"));
+    .pipe(changed("./docs/min/js"))
+    .pipe(dest("./docs/min/js"));
 };
 
 //監視
 const watchTask = () => {
-  watch("./src/**/*.html", series(htmlTask, browsersyncReload));
-  watch("./src/img/**", series(imageTask, browsersyncReload));
-  watch("./src/scss/**/*.scss", series(scssTask, browsersyncReload));
-  watch("./src/js/**/*.js", series(jsTask, browsersyncReload));
+  watch("./docs/scss/**/*.scss", series(scssTask, browsersyncReload));
+  watch("./docs/js/**/*.js", series(jsTask, browsersyncReload));
 };
 
 //browsersyncの開始
@@ -71,6 +48,6 @@ const browsersyncReload = (cb) => {
   cb();
 };
 
-exports.run = series(htmlTask, imageTask, scssTask, jsTask, browsersyncServe, watchTask);
-exports.watch = series(htmlTask, imageTask, scssTask, jsTask, watchTask)
-exports.build = series(htmlTask, imageTask, scssTask, jsTask);
+exports.run = series(scssTask, jsTask, browsersyncServe, watchTask);
+exports.watch = series(scssTask, jsTask, watchTask)
+exports.build = series(scssTask, jsTask);
