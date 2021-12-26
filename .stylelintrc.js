bcf1905d8936c69d7fccfb355399b1aecc6be600
Naruse Nia <notshinsin0000@gmail.com>
2021-12-27 01:38:17 +0900

    ESLint・Prettierセットアップ

---
+++ .stylelintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  plugins: [
+    "stylelint-scss",
+  ],
+  extends: [
+    "stylelint-config-recess-order",
+    "stylelint-config-prettier",
+    "stylelint-config-standard",
+    "stylelint-config-recommended-scss",
+  ],
+  rules: {
+    "selector-class-pattern": false,
+    "font-family-no-missing-generic-family-keyword": false,
+    "no-descending-specificity": false,
+    "selector-type-no-unknown": { ignore: ["ui"] },
+  },
+  ignoreFiles: [
+    "**/docs/css/style.min.css",
+    "**/node_modules/**",
+  ],
+};
