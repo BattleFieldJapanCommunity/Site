4abf42ef21b014411bac12f32f0cf2033f28b205
Naruse Nia <notshinsin0000@gmail.com>
2021-12-27 01:19:55 +0900

    ESLint + Prettierのセットアップ

--- docs/js/behavior.js
+++ docs/js/behavior.js
@@ -1,65 +1,66 @@
-//Fpr Animation
-const animationTargets = document.querySelectorAll(".animate, .animate-right");
-const site = document.querySelector(".site_container");
-const scrollOffset = 0.6;
+/* eslint-disable no-undef */
+// Fpr Animation
+const animationTargets = document.querySelectorAll('.animate, .animate-right')
+const site = document.querySelector('.site_container')
+const scrollOffset = 0.6
 
-//Fot hamburger Manu
-const header = document.querySelector("#header")
-const hamburger = document.querySelector(".nav_hamburger");
-const navigationMenu = document.querySelector(".nav_list");
+// Fot hamburger Manu
+const header = document.querySelector('#header')
+const hamburger = document.querySelector('.nav_hamburger')
+const navigationMenu = document.querySelector('.nav_list')
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navigationMenu.classList.toggle("active");
-  header.classList.toggle("active");
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active')
+  navigationMenu.classList.toggle('active')
+  header.classList.toggle('active')
 })
 
-navigationMenu.addEventListener("click", () => {
-  if (navigationMenu.classList.contains("active")) {
-    hamburger.classList.toggle("active");
-    navigationMenu.classList.toggle("active");
-    header.classList.toggle("active");
+navigationMenu.addEventListener('click', () => {
+  if (navigationMenu.classList.contains('active')) {
+    hamburger.classList.toggle('active')
+    navigationMenu.classList.toggle('active')
+    header.classList.toggle('active')
   }
 })
 
-//Animate when scroll//
+// Animate when scroll//
 const animate = () => {
-  console.log("called");
+  console.log('called')
   animationTargets.forEach((target) => {
-    const dist =
-      target.getBoundingClientRect().top + target.clientHeight * scrollOffset;
+    const dist = target.getBoundingClientRect().top + target.clientHeight * scrollOffset
 
     if (window.innerHeight <= dist) {
-      target.classList.remove("shown");
+      target.classList.remove('shown')
     }
     if (window.innerHeight > dist) {
-      target.classList.add("shown");
+      target.classList.add('shown')
     }
-  });
-};
+  })
+}
 
-//Header color change//
+// Header color change//
 const headerAnimate = () => {
-  const header = document.querySelector(".header");
-  const aboutus = document.querySelector(".aboutus");
-  const dist = aboutus.getBoundingClientRect().top;
+  const header = document.querySelector('.header')
+  const aboutus = document.querySelector('.aboutus')
+  const dist = aboutus.getBoundingClientRect().top
 
   if (window.innerHeight <= dist) {
-    header.classList.remove("header_white");
+    header.classList.remove('header_white')
   }
   if (window.innerHeight > dist) {
-    header.classList.add("header_white");
+    header.classList.add('header_white')
   }
-};
+}
 
-headerAnimate();
-animate();
+headerAnimate()
+animate()
 
-var scoll = new SmoothScroll("body", {
+// eslint-disable-next-line no-unused-vars
+const scoll = new SmoothScroll('body', {
   speed: 500,
-});
+})
 
-site.addEventListener("scroll", () => {
-  headerAnimate();
-  animate();
-});
+site.addEventListener('scroll', () => {
+  headerAnimate()
+  animate()
+})
--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,52 @@
-const { src, dest, watch, series } = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
-const postcss = require("gulp-postcss");
-const terser = require("gulp-terser");
-const changed = require("gulp-changed");
-const imagemin = require("gulp-imagemin");
-const mozjpeg = require("imagemin-mozjpeg");
-const pngquant = require("imagemin-pngquant");
-const cssnano = require("cssnano");
-const browsersync = require("browser-sync");
+const { src, dest, watch, series } = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const postcss = require('gulp-postcss')
+const terser = require('gulp-terser')
+const changed = require('gulp-changed')
+const cssnano = require('cssnano')
+const browsersync = require('browser-sync')
 
-//scssからcssに変換
+// scssからcssに変換
 const scssTask = () => {
-  return src("./docs/scss/**/*.scss")
+  return src('./docs/scss/**/*.scss')
     .pipe(sass())
     .pipe(postcss([cssnano()]))
-    .pipe(changed("./docs/css"))
-    .pipe(dest("./docs/css"));
-};
-
-//JSファイルの最適化
-const jsTask = () => {
-  return src("./docs/js/**/*.js")
-    .pipe(terser())
-    .pipe(changed("./docs/js/"))
-    .pipe(dest("./docs/js/min/"));
-};
-
-//監視
-const watchTask = () => {
-  watch("./docs/scss/**/*.scss", series(scssTask, browsersyncReload));
-  watch("./docs/js/**/*.js", series(jsTask, browsersyncReload));
-};
-
-const watchJSTask = () => {
-  watch("./docs/js/**/*.js", series(jsTask));
+    .pipe(changed('./docs/css'))
+    .pipe(dest('./docs/css'))
 }
 
-//browsersyncの開始
+// JSファイルの最適化
+const jsTask = () => {
+  return src('./docs/js/**/*.js').pipe(terser()).pipe(changed('./docs/js/')).pipe(dest('./docs/js/min/'))
+}
+
+// 監視
+const watchTask = () => {
+  watch('./docs/scss/**/*.scss', series(scssTask, browsersyncReload))
+  watch('./docs/js/**/*.js', series(jsTask, browsersyncReload))
+}
+
+const watchJSTask = () => {
+  watch('./docs/js/**/*.js', series(jsTask))
+}
+
+// browsersyncの開始
 const browsersyncServe = (cb) => {
   browsersync.init({
     server: {
-      baseDir: "./docs",
+      baseDir: './docs',
     },
-  });
-  cb();
-};
+  })
+  cb()
+}
 
-//browsersyncのリロード
+// browsersyncのリロード
 const browsersyncReload = (cb) => {
-  browsersync.reload();
-  cb();
-};
+  browsersync.reload()
+  cb()
+}
 
-exports.run = series(scssTask, jsTask, browsersyncServe, watchTask);
+exports.run = series(scssTask, jsTask, browsersyncServe, watchTask)
 exports.watch = series(scssTask, jsTask, watchTask)
 exports.watchJS = series(jsTask, watchJSTask)
-exports.build = series(scssTask, jsTask);
+exports.build = series(scssTask, jsTask)
